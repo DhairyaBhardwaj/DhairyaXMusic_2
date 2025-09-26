e26ac063b710474a88ac34ec18029a51752cb048
Dhairya Bhardwaj <156906679+DhairyaBhardwaj@users.noreply.github.com>
2025-09-26 19:19:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "22627587"))
 API_HASH = getenv("API_HASH", "4f664b2a2bf3b095a689a51780a45a5e")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-OWNER_ID = int(getenv("OWNER_ID", 5968801459))
+OWNER_ID = int(getenv("OWNER_ID", "5968801459"))
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Dhairya_bh")
 BOT_USERNAME = getenv("BOT_USERNAME", "Dhairya_music_2bot")
 BOT_NAME = getenv("BOT_NAME", "˹𝑫𝒉𝒂𝒊𝒓𝒚𝒂 𝑿 𝑴𝒖𝒔𝒊𝒄 2˼ ♪")
@@ -19,7 +19,7 @@ ASSUSERNAME = getenv("ASSUSERNAME", "musicxannie")
 
 # ── Database & logging ─────────────────────────────────────────────────────────
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-LOGGER_ID = int(getenv("LOGGER_ID", -1003169727261))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1003169727261"))
 
 # ── Limits (durations in min/sec; sizes in bytes) ──────────────────────────────
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
